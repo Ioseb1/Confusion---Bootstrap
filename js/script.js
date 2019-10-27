badeb09e88644b8120c323f964863225aa996c21
Ioseb Matiashvili <imatiashvili3@gmail.com>
2019-10-27 15:13:35 +0400

    4 Assignment

---
+++ js/script.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+    $("#tableButton").click(function() {
+        $("#reserveModal").modal('show');
+    });
+
+    $("#loginButton").click(function (){
+        $("#loginModal").modal('show');
+    })
+});
