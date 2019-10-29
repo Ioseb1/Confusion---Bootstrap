a42329e3f4b3fd7249ee0292476fe4fa07101cc7
Ioseb Matiashvili <imatiashvili3@gmail.com>
2019-10-29 12:33:38 +0400

    Modified script.js and index.html

--- js/script.js
+++ js/script.js
@@ -1,4 +1,20 @@
 $(document).ready(function () {
+    $('#mycarousel').carousel({interval: 2000});
+
+    // Pause and Play Button Goes Here(Changes Condition)
+    $('#carouselButton').click(function() {
+        if ($('#carouselButton').children('span').hasClass('fa-pause')) {
+        $('#mycarousel').carousel('pause');
+        $('#carouselButton').children('span').removeClass('fa-pause');
+        $('#carouselButton').children('span').addClass('fa-play');
+        }
+        else if ($('#carouselButton').children('span').hasClass('fa-play')) {
+        $('#mycarousel').carousel('cycle');
+        $('#carouselButton').children('span').removeClass('fa-play');
+        $('#carouselButton').children('span').addClass('fa-pause');
+        }
+    });
+    
     $("#tableButton").click(function() {
         $("#reserveModal").modal('show');
     });
